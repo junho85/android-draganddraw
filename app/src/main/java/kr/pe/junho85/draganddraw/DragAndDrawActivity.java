1d7f4e3b1b41fe3fc246f0d57be37297095bdf5f
june.kim <junho85@gmail.com>
2017-02-15 21:36:26 +0900

    first commit

---
+++ app/src/main/java/kr/pe/junho85/draganddraw/DragAndDrawActivity.java
@@ -0,0 +1,12 @@
+package kr.pe.junho85.draganddraw;
+
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DragAndDrawActivity extends SingleFragmentActivity {
+    @Override
+    protected Fragment createFragment() {
+        return DragAndDrawFragment.newInstance();
+    }
+}
